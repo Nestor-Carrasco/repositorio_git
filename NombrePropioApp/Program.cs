aa858c3f80400177fd85e78ad3007f0869983000
Nestor-Carrasco <aqguila00@gmail.com>
2019-09-22 00:27:40 -0300

    codigo correcto

--- NombrePropioApp/Program.cs
+++ NombrePropioApp/Program.cs
@@ -10,15 +10,22 @@ namespace NombrePropioApp
     {
         static void Main(string[] args)
         {
-            string nombre = "nesTOr miGUel cArrAscO hUEntEMil";
+            string nombre = "arIEl jA cID carDENAs";
             int a = nombre.IndexOf(" ") + 1; // posicion de 1° caracter despues de 1° caracter nulo de izquierda a derecha
-            int b = nombre.LastIndexOf(" "); // posicion de 1° caracter nulo de derecha a izquierda
+
+            string primerNombre = nombre.Substring(a);
+            int b = a + primerNombre.IndexOf(" ") + 1;
+
+            int c = nombre.LastIndexOf(" "); // posicion de 1° caracter nulo de derecha a izquierda
+
             Console.WriteLine(nombre.Substring(0, 1).ToUpper() + // 1° mayuscula 
                               nombre.Substring(1, (a - 1)).ToLower() + // minuscula
                               nombre.Substring(a, 1).ToUpper() + // 2° mayuscula
-                              nombre.Substring((a + 1), (b - a)).ToLower() + // minusculas
-                              nombre.Substring((b + 1), 1).ToUpper() + // 3° mayuscula
-                              nombre.Substring(b + 2).ToLower()); // minusculas
+                              nombre.Substring((a + 1), (b - a - 1)).ToLower() +// minusculas
+                              nombre.Substring(b, 1).ToUpper() + // 3° mayuscula
+                              nombre.Substring((b + 1), (c - b)).ToLower() + // minuscula
+                              nombre.Substring((c + 1), 1).ToUpper() + // 4° mayuscula
+                              nombre.Substring(c + 2).ToLower()); // minusculas
             Console.ReadKey();
         }
     }
